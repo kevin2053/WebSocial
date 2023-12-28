ed868763cd820c4d5c2f5fa7738da08226e40c13
Kevin <141423888+kevin2053@users.noreply.github.com>
2023-12-28 20:39:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const jsonFile = require('./app.json')
+const path = require('path');
+
+app.set("views", path.join(__dirname, "views"));
+
+app.set('view engine', 'ejs');
+
+app.get('/',  async (req, res) => {
+  res.render('home.ejs', {data: jsonFile})
+})
+
+app.listen(port, () => {
+  console.log("app is listening...")
+})
