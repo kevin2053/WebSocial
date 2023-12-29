e2d2c7ca73dceb672b66a0186450ddb9d5cc4004
Kevin <141423888+kevin2053@users.noreply.github.com>
2023-12-29 16:29:08 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const port = 3000
 const jsonFile = require('./app.json')
 const path = require('path');
 
+//<%= data.utenti[1].nome %
+
 app.set("views", path.join(__dirname, "views"));
 
 app.set('view engine', 'ejs');
